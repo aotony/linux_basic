cf8a8600c86198651439f3492abe54f908534ef5
aotony <myzhao.sg@gmail.com>
2016-09-27 09:58:30 +0800

    Finish the left part of experiment 3. Create file whoami.c and attach testlab2.c and testlab2.sh

---
+++ oslab/3_sys_call/whoami.c
@@ -0,0 +1,13 @@
+﻿/**
+ * iam.c
+ * the function call in file system
+ */
+#include <who.h>
+
+void main(void)
+{
+	char output[24];
+	int result;
+	result=whoami(output, 24);
+	printf("%s\n", output);
+}
