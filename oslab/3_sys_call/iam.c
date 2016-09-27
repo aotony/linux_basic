8be98271ef02eb3c92da7449ad48482851ffab5c
aotony <myzhao.sg@gmail.com>
2016-09-27 09:54:00 +0800

    In experiemnt 3: finish iam.c in hdc/usr/root/

---
+++ oslab/3_sys_call/iam.c
@@ -0,0 +1,20 @@
+﻿/**
+ * iam.c
+ * the function call in file system
+ */
+#include <who.h>
+
+void main(int argc, char **argv)
+{
+	int result;
+	if (argc==1)
+	{
+		result=iam("");
+		printf("input empty string, output %d\n", result);
+	}
+	else
+	{
+		result=iam((char *)argv[1]);
+		printf("input %s, output %d\n", (char *)argv[1], result);
+	}
+}
